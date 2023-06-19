7868bdce541d2a3d505745c56479140fd52cfa57
jacb <jacb@DESKTOP-U3QQ14A>
2023-06-19 12:32:12 +0200

    Ej 3 SIN realizar

---
+++ src/Ej_2/Ejercicio03.java
@@ -0,0 +1,41 @@
+// Algoritmo que pida tres números distintos de tipo float e indique cual es el que ocupa la posición central si los tres se ordenasen. 
+
+package Ej_2;
+
+//import java.util.Scanner;
+
+public class Ejercicio03 {
+
+	public static void main(String[] args) {
+
+		/*Scanner teclado= new Scanner(System.in);
+
+		float num1;
+		float num2;
+		float num3;
+		
+		float n_medio;
+		
+		System.out.print("Intro numero 1: ");
+		num1 = teclado.nextFloat();
+		System.out.print("Intro numero 2: ");
+		num2 = teclado.nextFloat();
+		System.out.print("Intro numero 2: ");
+		num3 = teclado.nextFloat();
+		
+		if (num1 == num2 || num1 == num3 || num2 == num3) {
+			System.out.println("¡¡Datos incorrectos!!");
+		}
+		
+		if (num1 < num2) {
+			if (num2 < num3) {
+				n_medio = num2;
+			} else if n_medio			}
+		else if ((num1 > num2)) 
+			n_medio = num1;
+		
+		System.out.println("El numero intermedio es: " + n_medio);
+		
+		teclado.close();*/
+	}
+}
